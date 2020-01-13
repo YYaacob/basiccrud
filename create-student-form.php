2f00c39cd6590782f65ec6b750381fe02b3aaf01
Yaacob <yyaacob0@gmail.com>
2020-01-13 17:27:52 +0100

    C-R of CRUD app is ready, create-student.php can add new data to the db

--- create-student-form.php
+++ create-student-form.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+<?php include "templates/header.php"; ?>
 <body>
     <form action="create-student.php" method="post">
         Voornaam: <input type="text" name="voornaam">
@@ -13,4 +6,4 @@
         <input type="submit" name="submit" value="Send">
     </form>
 </body>
-</html>
+<?php include "templates/footer.php" ?>
--- create-student.php
+++ create-student.php
@@ -1,4 +1,17 @@
 <?php
+
 $voornaam = $_POST['voornaam'];
 $achternaam = $_POST['achternaam'];
-echo "Hello, $voornaam $achternaam";
+
+
+//echo "Hello, $voornaam $achternaam";
+
+try {
+    include "connectdb.php";
+    $sql = "INSERT INTO student (voornaam, achternaam) VALUES ('$voornaam', '$achternaam')";
+    $db->exec($sql);
+    echo "New record created succesfully";
+} catch(PDOException $err) {
+    echo $sql . "<br />" . $err->getMessage();
+}
+
