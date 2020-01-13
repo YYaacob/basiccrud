3e75edda996e6cb7c7993fc650cb2f3cc4959e63
Yaacob <yyaacob0@gmail.com>
2020-01-13 18:20:38 +0100

    Fixing small things, typos, etc

--- create-student-form.php
+++ create-student-form.php
@@ -3,12 +3,12 @@
     <div class="container">
         <form action="create-student.php" method="post">
             <div class="form-group">
-                <label for="voornaam">Voornaam</label>
+                <label for="voornaam">Voornaam: </label>
                 <input class="form-control" type="text" id="voornaam" name="voornaam">
             </div>
 
             <div class="form-group">
-                <label for="achternaam"></label>
+                <label for="achternaam">Achternaam: </label>
                 <input class="form-control" type="text" id="achternaam" name="achternaam">
             </div>
 
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include "templates/header.php"; ?>
 <body>
     <div class="container">
-        <a href="create-student-form.php" class="btn btn-secondary mt-2 mb-2">Student toevoegen</a>
+        <a class="btn btn-secondary mt-2 mb-2" href="create-student-form.php">Student toevoegen</a>
         <?php include "list-students.php"; ?>
     </div>
 </body>
